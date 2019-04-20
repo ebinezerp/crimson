6bc9e02cdc193186c5d713a4ca6f93ecaf28024c
ebinezer.p87@gmail.com <ebinezer.p87@gmail.com>
2019-04-20 15:33:39 +0530

    profiles added

--- src/test/java/crimson/application/UserTest.java
+++ src/test/java/crimson/application/UserTest.java
@@ -1,4 +1,6 @@
-package crimson.application.frontend;
+package crimson.application;
+
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -6,11 +8,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-public class FrontendApplicationTests {
+@SpringBootTest(classes=FrontendApplication.class)
+public class UserTest {
 
 	@Test
-	public void contextLoads() {
+	public void test() {
+		assertTrue(true);
 	}
-
 }
