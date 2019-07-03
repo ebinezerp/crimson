50d49fc467fe3b1f163bfc8ae7f7711c2e2875ab
ebinezer.p87@gmail.com <ebinezer.p87@gmail.com>
2019-07-03 19:48:09 +0530

    product-description type from varchar to text

--- src/main/java/crimson/application/model/Product.java
+++ src/main/java/crimson/application/model/Product.java
@@ -4,12 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Transient;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import org.hibernate.annotations.Type;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.Data;
@@ -31,6 +33,7 @@ public class Product {
 	@NotBlank(message = "Product Description should not be blank")
 	@NotNull(message = "Product Description should not be null")
 	@Size(min = 30, message = "Product Description should be atleast 30 characters")
+	@Type(type="text")
 	private String productDescription;
 	
 	
